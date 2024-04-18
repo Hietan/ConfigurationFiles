08633ff40d52c53e8509b1273719de7b88b64544
KazumaYamasaki <yamasaki.kazuma.yj9@naist.ac.jp>
2024-04-18 11:32:39 +0900

    Add NeoVim

--- make_symboliclink.sh
+++ make_symboliclink.sh
@@ -7,7 +7,12 @@ ln -sf ${shell_dir}/Zsh/.zshrc ~
 ln -sf ${shell_dir}/Zsh/.zprofile ~
 
 # Oh My Zsh
+mkdir -p ~/.oh-my-zsh/themes
 ln -sf ${shell_dir}/OhMyZsh/hietan.zsh-theme ~/.oh-my-zsh/themes
 
 # Vim
 ln -sf ${shell_dir}/Vim/.vimrc ~
+
+# NeoVim
+mkdir -p ~/.config/nvim
+ln -sf ${shell_dir}/Vim/.vimrc ~/.config/nvim/init.vim
